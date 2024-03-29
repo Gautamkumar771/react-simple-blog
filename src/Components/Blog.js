93504ca28dede4b10e6a06ad6511c5f0837d82ec
Gautam kumar <gautamkumar844506@gmail.com>
2024-03-30 00:14:49 +0530

    usestate feacture

---
+++ src/Components/Blog.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+
+//Blogging App using Hooks
+export default function Blog(){
+    
+    //Passing the synthetic event as argument to stop refreshing the page on submit
+    function handleSubmit(e){
+        e.preventDefault();
+
+        setBlogs([{title,Content},...blogs])
+    }
+
+
+
+    const [title, setTitle] = useState("")
+    const [Content, setContent] = useState("")
+    const [blogs, setBlogs ] = useState([])
+
+    return(
+        <>
+        {/* Heading of the page */}
+        <h1>Write a Blog!</h1>
+
+        {/* Division created to provide styling of section to the form */}
+        <div className="section">
+
+        {/* Form for to write the blog */}
+            <form onSubmit={handleSubmit}>
+
+                {/* Row component to create a row for first input field */}
+                <Row label="Title">
+                        <input className="input"
+                                placeholder="Enter the Title of the Blog here.."
+                                value = {title}
+                                onChange = {(e) => setTitle(e.target.value)}
+                                />
+                </Row >
+
+                {/* Row component to create a row for Text area field */}
+                <Row label="Content">
+                        <textarea className="input content"
+                                placeholder="Content of the Blog goes here.."
+                                value = {Content}
+                                onChange = {(e)=> setContent(e.target.value)}
+                                />
+                </Row >
+
+                {/* Button to submit the blog */}            
+                <button className = "btn">ADD</button>
+            </form>
+                     
+        </div>
+
+        <hr/>
+
+        {/* Section where submitted blogs will be displayed */}
+        <h2> Blogs </h2>
+        {/* <h3>{title}</h3>
+        <p>{Content}</p> */}
+        {blogs.map((blog,i) =>(
+            <div className="blog" key={i}>
+                <h3>{blog.title}</h3>
+                <p>{blog.Content}</p>
+            </div>
+        ))}
+        
+        </>
+        )
+    }
+
+//Row component to introduce a new row section in the form
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <label>{label}<br/></label>
+        {props.children}
+        <hr />
+        </>
+    )
+}
